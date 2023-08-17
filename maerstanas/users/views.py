6f6e0d3155f9578ccb67764943ec25c175327c09
jaerrib <jaerrib@tutanota.com>
2023-08-17 12:57:43 -0400

    Add placeholder login/reg pages

--- maerstanas/users/views.py
+++ maerstanas/users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
 
@@ -8,15 +8,32 @@ def index(request):
 
 
 def login(request):
-    return HttpResponse('route test')
+    return render(request, 'login.html')
 
 
 def login_process(request):
-    return HttpResponse('route test')
+    errors = User.objects.login_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+            return redirect("/")
+    else:
+        user = User.objects.filter(email=request.POST['email'])
+        if user:
+            logged_user = user[0]
+            if bcrypt.checkpw(request.POST['password'].encode(),
+                              logged_user.password.encode()):
+                request.session['userid'] = logged_user.id
+                return redirect('/dashboard/')
+            else:
+                errors["password"] = "Invalid password"
+            for key, value in errors.items():
+                messages.error(request, value)
+                return redirect("/")
 
 
 def registration(request):
-    return HttpResponse('route test')
+    return render(request, 'register.html')
 
 
 def registration_process(request):
