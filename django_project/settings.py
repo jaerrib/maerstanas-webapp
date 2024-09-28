449617e83e9266b0a67eca4c6a16b148d9017bbc
jaerrib <jaerrib@tutanota.com>
2024-09-28 16:29:22 -0400

    Set up accounts app with custom user model and tests

--- django_project/settings.py
+++ django_project/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Local
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.CustomUser"
