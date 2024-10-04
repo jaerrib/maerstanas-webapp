50ce20ca036b99dc75164c7a0b2430f75e4225ab
jaerrib <jaerrib@tutanota.com>
2024-10-04 12:37:14 -0400

    Fix template path

--- games/views.py
+++ games/views.py
@@ -12,25 +12,25 @@ from .models import Game
 class GameListView(ListView):
     model = Game
     context_object_name = "game_list"
-    template_name = "games/game_list.html"
+    template_name = "game/game_list.html"
 
 
 class GameDetailView(DetailView):
     model = Game
     context_object_name = "game"
-    template_name = "games/game_detail.html"
+    template_name = "game/game_detail.html"
 
 
 class GameCreateView(CreateView):
     model = Game
-    template_name = "games/game_create.html"
+    template_name = "game/game_create.html"
 
 
 class GameUpdateView(UpdateView):
     model = Game
-    template_name = "games/game_update.html"
+    template_name = "game/game_update.html"
 
 
 class GameDeleteView(DeleteView):
     model = Game
-    template_name = "games/game_delete.html"
+    template_name = "game/game_delete.html"
