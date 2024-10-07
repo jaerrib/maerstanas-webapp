5835c61bbb80894597351a625ba11635f82e3b71
jaerrib <jaerrib@tutanota.com>
2024-10-07 17:23:36 -0400

    Add models to Django admin

--- games/admin.py
+++ games/admin.py
@@ -1,1 +1,34 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Game, GameBoard, MovesLeftList, PlayedMovesList
+
+
+class GameBoardAdmin(admin.ModelAdmin):
+    list_display = ["id"]
+
+
+class PlayedMovesListAdmin(admin.ModelAdmin):
+    list_display = ["id"]
+
+
+class MovesLeftListAdmin(admin.ModelAdmin):
+    list_display = ["id"]
+
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "name",
+        "player1",
+        "player2",
+        "score_p1",
+        "score_p2",
+        "using_special_stones",
+        "using_standard_scoring",
+    ]
+
+
+admin.site.register(Game, GameAdmin)
+admin.site.register(GameBoard, GameBoardAdmin)
+admin.site.register(MovesLeftList, MovesLeftListAdmin)
+admin.site.register(PlayedMovesList, PlayedMovesListAdmin)
