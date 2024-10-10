4018d19104b163ee9e2c461f63a17fa57bdead62
jaerrib <jaerrib@tutanota.com>
2024-10-10 12:33:20 -0400

    Add absolute URL to custom user for profile usage

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ import uuid
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
 
 class CustomUser(AbstractUser):
@@ -11,3 +12,6 @@ class CustomUser(AbstractUser):
     games_lost = models.IntegerField(default=0)
     games_tied = models.IntegerField(default=0)
     games_abandoned = models.IntegerField(default=0)
+
+    def get_absolute_url(self):
+        return reverse("userprofile_detail", args=[str(self.id)])
