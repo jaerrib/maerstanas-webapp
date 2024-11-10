e08e076562929c208b6bbd6f410a60eacf52448d
jaerrib <jaerrib@tutanota.com>
2024-11-10 10:13:10 -0500

    Update Django admin for invitations and system notices

--- game_messages/admin.py
+++ game_messages/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 
 from .models import Invitation, SystemNotice
 
-admin.site.register(Invitation)
-admin.site.register(SystemNotice)
+
+class CustomInvitationAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "game",
+        "sender",
+        "receiver",
+    ]
+
+class CustomSystemNoticeAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "user",
+        "message_text",
+    ]
+
+admin.site.register(Invitation, CustomInvitationAdmin)
+admin.site.register(SystemNotice, CustomSystemNoticeAdmin)
