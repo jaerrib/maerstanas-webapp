8497eac59271a2b34a54e979bf8c4f7cbf2e0f8a
jaerrib <jaerrib@tutanota.com>
2024-11-10 09:57:32 -0500

    Add model to handle system notifications

--- game_messages/admin.py
+++ game_messages/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Invitation
+from .models import Invitation, SystemNotice
 
 admin.site.register(Invitation)
+admin.site.register(SystemNotice)
