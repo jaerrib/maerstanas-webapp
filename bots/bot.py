a71d51ab9f31cbf967d38679e656000d9a56d5d0
jaerrib <jaerrib@tutanota.com>
2024-12-06 19:29:05 -0500

    Add logic to handle multiple bot types with placeholders

--- bots/bot.py
+++ bots/bot.py
@@ -14,13 +14,29 @@ def play_computer_move(game_state):
     if not game_state.game_over and game_rules.player_must_pass(game_state):
         game_state = game_rules.pass_player_turn(game_state)
     else:
-        comp_move = secrets.choice(game_state.moves_left_list)
-        row, col = comp_move[0], comp_move[1]
-        stone = 1
-        game_state = game_rules.assign_move(game_state, stone, row, col)
+        match game_state.player2.username:
+            case "Oswin":
+                move = play_random_move(game_state)
+            # placeholder for other bots
+            # case "Aelfric":
+            #     move = play_random_move(game_state)
+            # placeholder for other bots
+            # case "Aethelstan":
+            #     move = play_random_move(game_state)
+            case _:
+                move = play_random_move(game_state)
+        game_state = game_rules.assign_move(
+            game_state, move["stone"], move["row"], move["col"]
+        )
         game_state.moves_left_list = game_rules.remaining_standard_moves(
             game_state.gameboard["data"]
         )
         game_state = game_rules.change_player(game_state)
         game_state.game_over = game_rules.is_game_over(game_state)
     return game_state
+
+
+def play_random_move(game_state):
+    comp_move = secrets.choice(game_state.moves_left_list)
+    move = {"row": comp_move[0], "col": comp_move[1], "stone": 1}
+    return move
